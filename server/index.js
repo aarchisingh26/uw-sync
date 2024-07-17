f09b829c69bf2f7699d1c6cd3ef6f21d82f5f44b
Aarchi Singh <a254sing@uwaterloo.ca>
2024-07-17 19:35:00 -0400

    bug fixes

--- server/index.js
+++ server/index.js
@@ -10,13 +10,21 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://uw-sync.vercel.app'
   }));
-  
+
+
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://uw-sync.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
+  
+  // Define your routes
+  app.get('/exams', (req, res) => {
+    // Your endpoint logic here
+    res.json({ message: 'CORS enabled' });
+  });
+  
   
 
 //ROUTES
