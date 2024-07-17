1135212f928d39ec67e58ee776688ab54244f537
Aarchi Singh <a254sing@uwaterloo.ca>
2024-07-17 19:29:23 -0400

    bug fixes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://uw-sync.vercel.app'
   }));
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://uw-sync.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 //ROUTES
 
