011ec520b9535d37d9b529ddc76ffe82ee813bb3
Aarchi Singh <a254sing@uwaterloo.ca>
2024-07-18 13:39:39 -0400

    bug fixes

--- server/index.js
+++ server/index.js
@@ -168,7 +168,7 @@ const cors = require("cors");
 const pool = require("./db");
 
 const corsOptions = {
-  origin: 'https://uw-sync.vercel.app/',
+  origin: 'https://uw-sync.vercel.app',
   credentials: true
 };
 
